c35988be4f1158e1e97c431d0ef4d3c64deafdb6
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-17 15:04:14 +0700

    Deleted unused imports

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ServiceSec from '../../sections/ServicesSec';
-import BannerSec from '../../sections/BannerSec';
 
 const ServicesPage = (props) => {
   const { paddingtop } = props;
